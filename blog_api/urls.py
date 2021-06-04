a58d742d011873414e650aea6969fc378b9635db
zhoomartq <zhomart.osmonaliev@gmail.com>
2021-06-04 09:16:15 +0600

    blog project

---
+++ blog_api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # TODO add
+    path('users/', views.UserListView.as_view()),
+    path('users/<int:pk>/', views.UserDetailView.as_view()),
+    path('posts/create/', views.PostCreateView.as_view()),
+    path('posts/', views.PostListView.as_view()),
+    path('posts/<int:pk>/', views.PostDetailView.as_view()),
+    path('posts/update/<int:pk>/', views.PostUpdateView.as_view()),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view()),
+    path('comments/', views.CommentListCreateView.as_view()),
+    path('comments/<int:pk>/', views.CommentDetailView.as_view()),
+    path('categories/', views.CategoryView.as_view()),
+
+
+
+
+]
